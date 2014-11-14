9f71d3b381ae83e85e177d9bcf0727692b5f799c
James Bennett <jwbennet@gmail.com>
2014-11-13 21:57:21 -0500

    Fixed initiator ID

--- etc/create_process.sh
+++ etc/create_process.sh
@@ -2,5 +2,5 @@
 # [{"id":"5465656561486138aeb4dfc7","name":"Process 1"},{"id":"5465658261486138aeb4dfc8","name":"Process 2"},{"id":"5465658461486138aeb4dfc9","name":"Process 3"},{"id":"5465658661486138aeb4dfca","name":"Process 4"},{"id":"5465658961486138aeb4dfcb","name":"Process 5"}]
 
 curl -H "Content-Type: application/json" \
-  --data '{"processId":"5465656561486138aeb4dfc7","initiatorId":"kellerj"}' \
+  --data '{"processId":"5465656561486138aeb4dfc7","initiatorId":"1"}' \
   http://localhost:8090/processInstances
